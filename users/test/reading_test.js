7fba8647e8258124d15c660ee16a9edd38af7128
Vikash Joshi <joshi.vikash17@gmail.com>
2019-06-28 22:16:42 +0530

    finding records by id

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -15,8 +15,18 @@ describe('Reading Records', ()=>{
 	it('asserting that only one user exists by comparing the _id property', (done)=>{
 		User.find({ name : 'Kapil'}).
 			then((users)=>{
+				// Here we cann't directly compare _id since its an object
 				assert(users[0]._id.toString() === kapil._id.toString());
 				done();
 			})
+	});
+	
+	it('finding a record by id', (done)=>{
+		// Here we donn't need to convert _id to String since mongoose knows how to deal with Object _id.
+		User.findOne({ _id : kapil._id}).
+			then((user)=>{
+				assert(user.name === 'Kapil');
+				done();
+			});
 	});
 });
